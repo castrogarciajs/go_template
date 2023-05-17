0ee350a825eb5722962c2292f984d82f67de4531
Sebastian009w <ia3266595@gmail.com>
2023-05-17 12:37:38 -0500

    estrutura en go

--- pkg/controllers/init.go
+++ pkg/controllers/init.go
@@ -3,10 +3,41 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/sebastian009w/go_template/app/database"
 	"github.com/sebastian009w/go_template/pkg/utils"
 )
 
+type Username struct {
+	ID    int
+	Name  string
+	Email string
+}
+
 func Init(w http.ResponseWriter, r *http.Request) {
+	conn := database.MySQL()
+
+	array, err := conn.Query("SELECT * FROM migo;")
+
+	username := Username{}
+	arrayUsername := []Username{}
+
+	for array.Next() {
+		var id int
+		var name, email string
+
+		err = array.Scan(&id, &name, &email)
+
+		if err != nil {
+			panic(err.Error())
+		}
+		username.ID = id
+		username.Name = name
+		username.Email = email
+
+	}
+	if err != nil {
+		panic(err.Error())
+	}
 
 	utils.Templates.ExecuteTemplate(w, "index", nil)
 
