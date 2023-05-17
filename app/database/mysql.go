18e5b3d7f5facc7e24e7f265ce7601435fa936bd
Sebastian009w <ia3266595@gmail.com>
2023-05-17 11:52:12 -0500

    coneccion a mysql

--- app/database/mysql.go
+++ app/database/mysql.go
@@ -1,1 +1,21 @@
 package app
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func MySQL() (conn *sql.DB) {
+	Driver := "mysql"
+	Username := "root"
+	Password := "sebastian123_*"
+	Name := "mygo"
+
+	conn, err := sql.Open(Driver, Username+":"+Password+Name)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	return conn
+}
