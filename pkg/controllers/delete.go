d87d479d27a1e1826ff9623b362db450aa0a2133
Sebastian009w <ia3266595@gmail.com>
2023-05-17 13:16:36 -0500

    eliminando de la tabla

--- pkg/controllers/delete.go
+++ pkg/controllers/delete.go
@@ -1,12 +1,20 @@
 package controllers
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/sebastian009w/go_template/app/database"
 )
 
 func Delete(w http.ResponseWriter, r *http.Request) {
 	ID := r.URL.Query().Get("id")
+	conn := database.MySQL()
+	delete, err := conn.Prepare("DELETE FROM gomi WHERE id = ?")
 
-	fmt.Println(ID)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	delete.Exec(ID)
+	http.Redirect(w, r, "/", http.StatusNoContent)
 }
