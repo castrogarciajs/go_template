e1a8f6435b588b61df6e907745e3f51feb6d5ef7
Sebastian009w <ia3266595@gmail.com>
2023-05-17 12:22:20 -0500

    insertando usuario

--- pkg/controllers/init.go
+++ pkg/controllers/init.go
@@ -3,20 +3,11 @@ package controllers
 import (
 	"net/http"
 
-	"github.com/sebastian009w/go_template/app/database"
 	"github.com/sebastian009w/go_template/pkg/utils"
 )
 
 func Init(w http.ResponseWriter, r *http.Request) {
-	conn := database.MySQL()
-
-	create, err := conn.Prepare("INSERT INTO gomi (name, email) VALUES ('sebastian', 'sebastian@example.com')")
 
 	utils.Templates.ExecuteTemplate(w, "index", nil)
-
-	if err != nil {
-		panic(err.Error())
-	}
-	create.Exec()
 
 }
