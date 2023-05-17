928a6f2678a8b62c6f9a809bce5e7d95827b40f5
Sebastian009w <ia3266595@gmail.com>
2023-05-17 11:42:14 -0500

    creacion de la tabla y la base de datos

--- app/database.sql
+++ app/database.sql
@@ -1,1 +1,16 @@
 CREATE DATABASE mygo;
+
+SHOW DATABASES;
+
+
+CREATE TABLE gomi(
+    id INT AUTO_INCREMENT,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+SHOW TABLES;
+
+INSERT INTO gomi (name, email) 
+VALUES ('Jhon Doe', 'jhondoe@example.com');
